97a1d3d5efc5140655ef937134b4cdaf5de1ae1d
phongthorn <phongthornk@rabbit.co.th>
2022-02-10 19:04:16 +0700

    Run fixer

--- src/StringHelper.php
+++ src/StringHelper.php
@@ -15,16 +15,16 @@ class StringHelper
      *
      * @return string
      */
-    public static function slugUtf8(string $string): string
+    public static function slugUtf8(string $string) : string
     {
         // Convert all underscores into separator
-        $string = preg_replace('!['.preg_quote('_').']+!u', self::SEPARATOR, $string);
+        $string = preg_replace('![' . preg_quote('_') . ']+!u', self::SEPARATOR, $string);
 
         // Remove all characters that are not the separator, letters, numbers, or whitespace.
-        $string = preg_replace('![^'.preg_quote(self::SEPARATOR).'\pL\pN\s\p{Thai}]+!u', '', $string);
+        $string = preg_replace('![^' . preg_quote(self::SEPARATOR) . '\pL\pN\s\p{Thai}]+!u', '', $string);
 
         // Replace all separator characters and whitespace by a single separator
-        $string = preg_replace('!['.preg_quote(self::SEPARATOR).'\s]+!u', self::SEPARATOR, $string);
+        $string = preg_replace('![' . preg_quote(self::SEPARATOR) . '\s]+!u', self::SEPARATOR, $string);
 
         $string = Str::lower($string);
 
